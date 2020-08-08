7521adc569125c1327089d0c624aa9f8586e50c7
Anh Khoa <68534134+vuanhkhoa2109@users.noreply.github.com>
2020-08-08 11:57:50 +0700

    feat: init project

---
+++ android/app/src/main/kotlin/com/example/i_am_rick/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.i_am_rick
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am not Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
